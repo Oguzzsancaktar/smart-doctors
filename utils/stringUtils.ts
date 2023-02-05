701c67452e55c5482f2dc84a5c703c22620c9482
Oğuz Taha Sancaktar <46040028+Oguzzsancaktar@users.noreply.github.com>
2023-02-05 04:30:23 +0300

    New string utils added;

--- utils/stringUtils.ts
+++ utils/stringUtils.ts
@@ -7,3 +7,25 @@ export const getFirstFiveWords = (str: string) => {
   const words = str.split(" ");
   return words.slice(0, 5).join(" ");
 };
+
+export const getFirstGivenChars = (str: string, chars: number) => {
+  return str.trim().slice(0, chars) + "...";
+};
+
+export const getFirstTwoCapitalLetters = (str: string) => {
+  const words = str.split(" ");
+  const firstWord = words[0];
+  const secondWord = words[1];
+  if (secondWord) {
+    firstWord?.charAt(0)?.toUpperCase() + secondWord?.charAt(0)?.toUpperCase();
+  }
+  return firstWord?.charAt(0)?.toUpperCase();
+};
+
+export const camelCaseToSentenceCase = (str: string) => {
+  return str.split(/(?=[A-Z])/).join(" ");
+};
+
+export const converUnderrscoreToSpace = (str: string) => {
+  return str.split("_").join(" ");
+};
